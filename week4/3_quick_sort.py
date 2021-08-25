a52c2e060ccf3173e2117db5c9e800c90b81a63e
thetweak <thetweak@cloud.upwork.com>
2021-08-25 15:53:54 +0300

    week 4, 3 quick sort

--- week4/3_quick_sort.py
+++ week4/3_quick_sort.py
@@ -4,22 +4,30 @@ import random
 
 def partition(a, l, r):
     j = l
+    j_ = l
+    k = l
     pivot = a[l]
     for i in range(l+1, r+1):
+        if a[i] == pivot:
+            j_ += 1
+            a[j_], a[i] = a[i], a[j_]
         if a[i] <= pivot:
             j += 1
             a[j], a[i] = a[i], a[j]
-    a[l], a[j] = a[j], a[l]
-    return j
+    m_size = j_
+    while j_ >= l:
+        a[j_], a[j] = a[j], a[j_]
+        j_ -= 1
+        j -= 1
+    return j+1, j+1+m_size
 
 
 def quick_sort(a, l, r):
     if l >= r:
         return
-    m = partition(a, l, r)
-    quick_sort(a, l, m-1)
-    quick_sort(a, m+1, r)
-
+    m1, m2 = partition(a, l, r)
+    quick_sort(a, l, m1-1)
+    quick_sort(a, m2+1, r)
 
 finput = fileinput.input()
 n = int(next(finput))
