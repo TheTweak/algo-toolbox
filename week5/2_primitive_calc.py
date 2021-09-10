2a236d28f395b1c40ab6b8255b3b70f06e9c7369
thetweak <thetweak@cloud.upwork.com>
2021-09-10 14:42:42 +0300

    week 5, primitive calc

--- week5/2_primitive_calc.py
+++ week5/2_primitive_calc.py
@@ -2,16 +2,20 @@ import fileinput
 
 
 def get_ops_n(n: int) -> int:
-    dp = [float('inf')] * (n+1)
-    dp[1] = 0
+    dp = [(float('inf'), [])] * (n+1)
+    dp[1] = (0, [1])
     for i in range(2, n+1):
-        plus_one = dp[i-1]+1
-        mult_two = float('inf') if i%2 else dp[i//2]+1
-        mult_three = float('inf') if i%3 else dp[i//3]+1
-        dp[i] = min(plus_one, mult_two, mult_three)
+        plus_one = dp[i-1]
+        mult_two = (float('inf'), []) if i%2 else dp[i//2]
+        mult_three = (float('inf'), []) if i%3 else dp[i//3]
+        variants = sorted([plus_one, mult_two, mult_three], key=lambda x: x[0])
+        opt_var = variants[0]
+        dp[i] = (opt_var[0]+1, opt_var[1]+[i])
     return dp[-1]
 
 
 if __name__ == '__main__':
     n = int(next(fileinput.input()))
-    print(get_ops_n(n))
+    x, path = get_ops_n(n)
+    print(x)
+    print(*path)
